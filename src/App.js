3d2978fbf3ee613fa4ff1a0fb8a36f454e2fd470
Theodore Pinto <theopinto98@gmail.com>
2022-06-26 10:22:38 +0800

    Add markers and data for each country

--- src/App.js
+++ src/App.js
@@ -1,56 +1,9 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
+import MapView from './components/mapView/MapView';
 import './App.css';
 
 function App() {
-  const sampleLocation =  {
-    "updated": 1656163358377,
-    "country": "Afghanistan",
-    "countryInfo": {
-        "_id": 4,
-        "iso2": "AF",
-        "iso3": "AFG",
-        "lat": 33,
-        "long": 65,
-        "flag": "https://disease.sh/assets/img/flags/af.png"
-    },
-    "cases": 182072,
-    "todayCases": 28,
-    "deaths": 7717,
-    "todayDeaths": 0,
-    "recovered": 164040,
-    "todayRecovered": 54,
-    "active": 10315,
-    "critical": 1124,
-    "casesPerOneMillion": 4478,
-    "deathsPerOneMillion": 190,
-    "tests": 996925,
-    "testsPerOneMillion": 24519,
-    "population": 40659086,
-    "continent": "Asia",
-    "oneCasePerPeople": 223,
-    "oneDeathPerPeople": 5269,
-    "oneTestPerPeople": 41,
-    "activePerOneMillion": 253.69,
-    "recoveredPerOneMillion": 4034.52,
-    "criticalPerOneMillion": 27.64
-}
-  return (
-      <MapContainer center={[sampleLocation.countryInfo.lat, sampleLocation.countryInfo.long]} zoom={13} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  <Marker position={[sampleLocation.countryInfo.lat, sampleLocation.countryInfo.long]}>
-    <Popup>
-      <h3>{sampleLocation.country}</h3>
-      <div>Confirmed: {sampleLocation.cases}</div>
-      <div>Deaths: {sampleLocation.deaths}</div>
-      <div>Last Updated: {new Date(sampleLocation.updated).toLocaleString()}</div>
-    </Popup>
-  </Marker>
-</MapContainer>   
-  );
+ return(<MapView/>)
 }
 
 export default App;
