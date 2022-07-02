9a16700ff315339c342f0490b763d4acf56a6924
Theodore Pinto <theopinto98@gmail.com>
2022-07-02 23:13:12 +0800

    Add ability to retrieve data based on search results

--- src/components/countryTally/CountryTally.js
+++ src/components/countryTally/CountryTally.js
@@ -3,29 +3,44 @@ import './CountryTally.css'
 import axios from 'axios'
 
 
+async function getData(countryString) {
+  if (countryString == "")
+    return await axios.get('https://disease.sh/v3/covid-19/countries?sort=cases')
+  return await axios.get('https://disease.sh/v3/covid-19/countries/' + countryString + '?yesterday')
+}
+
 class CountryTally extends Component {
   constructor(props) {
     super(props);
-    this.state = { countryTally: [] };
+    this.state = { countryTally: [], countrySearch: "" };
+    this.handleChange = this.handleChange.bind(this);
   }
 
   async componentDidMount() {
-    const countryTally = await axios.get('https://disease.sh/v3/covid-19/countries?sort=cases')
+    const countryTally = await getData("")
 
     this.setState({
       countryTally: countryTally.data,
     });
-    console.log(this.state.countryTally)
+  }
+
+  async handleChange(event) {    
+    const countryTally = await getData(event.target.value)
+    console.log(countryTally.data)
+    this.setState({
+      countryTally: countryTally.data,
+    });  
   }
 
   render() {
+    
     const CountryTallyRows = ({ countries }) => {
       return countries.map(country => {
         if (!country.countryInfo._id) return null
         return (
-          <tr>
+          <tr key={country.countryInfo._id}>
             <td>
-              <img className="flag" src={country.countryInfo.flag}/>
+              <img className="flag" src={country.countryInfo.flag} alt={country.countryInfo.iso3}/>
             </td>
             
             <td>{country.country}</td>
@@ -37,10 +52,13 @@ class CountryTally extends Component {
 
     return (
       <div className='container-country'>
-        <div>Hi</div>
+        <input contentEditable="true" className="search-bar" placeholder="Search Country" onChange={this.handleChange}/>
         <div className='container-table'>
         <table>
-          <CountryTallyRows countries={this.state.countryTally} />
+          <tbody>
+            <CountryTallyRows countries={this.state.countryTally} />
+          </tbody>
+          
         </table>
         </div>
       </div>
